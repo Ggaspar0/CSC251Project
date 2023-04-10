4d51c842ac2e76a3d50e0c2303d096db986f09bb
Gagik Gasparyan <ggaspar0@email.cpcc.edu>
2023-04-10 02:43:52 -0400

    added code to display all policy information

--- Project_Gagik_Gasparyan.java
+++ Project_Gagik_Gasparyan.java
@@ -1,59 +1,63 @@
-import java.util.Scanner; //import needed to create and use the scanner object which reads user input
+import java.util.*; //import needed to create and use the scanner object which reads user input or files
+import java.util.ArrayList;
+import java.io.*;
 
 public class Project_Gagik_Gasparyan {//class demonstrating attributes and behaviors of the Policy class
 
     public static void main(String[] s){  
 
-               //scanner object is created and named "getInput"
-    Scanner getInput = new Scanner(System.in);
+    try {
 
     //These are the variables in the main method that will store input values
     int policyNumber, holderAge;
-    double holderHeight, holderWeight;
+    double holderHeight=0.0, holderWeight=0.0;
     String providerName, holderFName, holderLName, smokerStatus;
+    ArrayList<Policy> policyList = new ArrayList<Policy>();
 
-   //a series of prompts in the form of output text, followed by the scanner object to read and save the input
-    System.out.print("Please enter the Policy Number: ");
-        policyNumber = getInput.nextInt();
-
-    System.out.print("Please enter the Provider Name: ");
-                      getInput.nextLine();//clear input buffer
-        providerName= getInput.nextLine();
-
-    System.out.print("Please enter the Policyholder's First Name: ");
-        holderFName = getInput.nextLine();
-
-    System.out.print("Please enter the Policyholder's Last Name: ");
-        holderLName=getInput.nextLine();
-
-    System.out.print("Please enter the Policyholder's Age: ");
-        holderAge= getInput.nextInt();
-
-    System.out.print("Please enter the Policyholder's Smoking status(smoker/non-smoker): ");
-                     getInput.nextLine();//clear input buffer
-        smokerStatus=getInput.nextLine();
-
-    System.out.print("Please enter the Policyholder's height(in inches): ");
-        holderHeight= getInput.nextDouble();
-
-    System.out.print("Please enter the Policyholder's weight(in pounds): ");
-        holderWeight= getInput.nextDouble();
+    //create a file object and link to the file txt in directory
+    //create a scanner and pass in the file object reference
+    File policyInformation = new File("PolicyInformation.txt");
+    Scanner readPolicyInformation = new Scanner(policyInformation);
+    //continue reading while there is any data left to read
+    while (readPolicyInformation.hasNext()){
+    
+      policyNumber = readPolicyInformation.nextInt();
+      providerName = readPolicyInformation.nextLine();
+      holderFName = readPolicyInformation.nextLine();              
+      holderLName = readPolicyInformation.nextLine();           
+      holderAge = readPolicyInformation.nextInt();     
+      smokerStatus = readPolicyInformation.nextLine();     
+      holderHeight = readPolicyInformation.nextInt();     
+      holderWeight = readPolicyInformation.nextInt();     
+         //if there is a blank line next read in blank line
+         if(readPolicyInformation.hasNext()){
+            readPolicyInformation.nextLine();
+         }
+         //create a policy object and pass values into the parameters
+         Policy policy = new Policy(policyNumber, providerName, holderFName, holderLName, holderAge, smokerStatus, holderHeight, holderWeight);
+         policyList.add(policy);//add the newly created policy object to the array list
+   
+    }readPolicyInformation.close();//close the file
+    
+    for(int i=0; i < policyList.size(); i++){
+    
+    System.out.println("\nPolicy Number: "+ policyList.get(i).getPolicyNumber());
+    System.out.println("\nProvider Name: "+ policyList.get(i).getProviderName());
+    System.out.println("\nPolicyholder's First Name: "+ policyList.get(i).getPolicyHolderFirstName());
+    System.out.println("\nPolicyholder's Last Name: "+ policyList.get(i).getPolicyHolderLastName());
+    System.out.println("\nPolicyholder's Age: "+ policyList.get(i).getPolicyHolderAge());
+    System.out.println("\nPolicyholder's Smoking Status: "+ policyList.get(i).getSmokingStatus());
+    System.out.printf("\nPolicyholder's Height: %.1f\n", policyList.get(i).getPolicyHolderHeight());
+    System.out.printf("\nPolicyholder's Weight: %.1f\n", policyList.get(i).getPolicyHolderWeight());
+    System.out.printf("\nPolicyholder;s BMI: %.2f\n", policyList.get(i).getHOLDER_BMI());
+    System.out.printf("\nPolicy Price: $%.2f\n", policyList.get(i).getPOLICY_PRICE());
+    
+    }
+  
         
-        
-    //now that all the values have been collected from the user, we instantiate a Policy object using all the arguments necessary
-    //this instance of the Policy class will be named "policy_1"
-    Policy policy_1 = new Policy(policyNumber,providerName,holderFName,holderLName,holderAge,smokerStatus,holderHeight,holderWeight);
+    }catch(IOException ex){
+    System.out.println("Error reading file"+ ex.getMessage());
+    }
 
-    //display all the information about policy_1, along with formatting
-    System.out.println("\nPolicy Number: "+ policy_1.getPolicyNumber());
-    System.out.println("\nProvider Name: "+ policy_1.getProviderName());
-    System.out.println("\nPolicyholder's First Name: "+ policy_1.getPolicyHolderFirstName());
-    System.out.println("\nPolicyholder's Last Name: "+ policy_1.getPolicyHolderLastName());
-    System.out.println("\nPolicyholder's Age: "+ policy_1.getPolicyHolderAge());
-    System.out.println("\nPolicyholder's Smoking Status: "+ policy_1.getSmokingStatus());
-    System.out.printf("\nPolicyholder's Height: %.1f\n", policy_1.getPolicyHolderHeight());
-    System.out.printf("\nPolicyholder's Weight: %.1f\n", policy_1.getPolicyHolderWeight());
-    System.out.printf("\nPolicyholder;s BMI: %.2f\n", policy_1.getHOLDER_BMI());
-    System.out.printf("\nPolicy Price: $%.2f\n", policy_1.getPOLICY_PRICE());
     }//end main
-}//end Project_Gagik_Gasparyan class
+} //end Project_Gagik_Gasparyan class
